abc26bb0b948329748c7d61981ad46fabd9123d3
Monikai6 <115080235+Monikai6@users.noreply.github.com>
2023-05-20 06:23:20 +0100

    Api testing and github actions

---
+++ cypress/e2e/step_definitions/apiStepDef.js
@@ -0,0 +1,90 @@
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor'
+const baseUrl = 'https://restful-booker.herokuapp.com';
+const username = 'admin';
+const password = 'password123';
+let authToken;
+let bookingId;
+
+Given('1. I am authenticated', () => {
+  cy.request({
+    method: 'POST',
+    url: `${baseUrl}/auth`,
+    body: {
+      username,
+      password
+    }
+  }).its('body.token').then((token) => {
+    authToken = token;
+    cy.log(`Token: ${authToken}`);
+  });
+});
+
+When('1. I send a {string} request to {string}', (method, url) => {
+  const updatedUrl = url.replace('{{bookingId}}', bookingId);
+  cy.request({
+    method,
+    url: `${baseUrl}${updatedUrl}`,
+    auth: {
+      username,
+      password
+    }
+  }).as('response');
+  cy.get('@response').then((response) => {
+    cy.log(`Response: ${JSON.stringify(response.body)}`);
+  });
+});
+
+When('2. I send a {string} request to {string} with request body:', (method, url, requestBody) => {
+    cy.request({
+      method,
+      url: `${baseUrl}${url}`,
+      auth: {
+        username,
+        password
+      },
+      body: JSON.parse(requestBody)
+    }).as('response');
+  });
+
+When('1. I send a {string} request to {string} with request body from fixture {string}', (method, url, fixtureName) => {
+  cy.fixture(fixtureName).then((fixtureData) => {
+    cy.request({
+      method,
+      url: `${baseUrl}${url}`,
+      auth: {
+        username,
+        password
+      },
+      body: fixtureData
+    }).as('response');
+    cy.get('@response').then((response) => {
+      cy.log(`Response: ${JSON.stringify(response.body)}`);
+    });
+  });
+});
+
+When('1. I send a DELETE request to {string}', (url) => {
+  const updatedUrl = url.replace('{{bookingId}}', bookingId);
+  cy.request({
+    method: 'DELETE',
+    url: `${baseUrl}${updatedUrl}`,
+    auth: {
+      username,
+      password
+    }
+  }).as('response');
+  cy.get('@response').then((response) => {
+    cy.log(`Response: ${JSON.stringify(response.body)}`);
+  });
+});
+
+Then('1. the response status code should be {int}', (statusCode) => {
+  cy.get('@response').its('status').should('equal', statusCode);
+});
+
+Then('1. the response should have the booking ID', () => {
+  cy.get('@response').its('body.bookingid').should('exist').then((id) => {
+    bookingId = id;
+    cy.log(`Booking ID: ${bookingId}`);
+  });
+});
